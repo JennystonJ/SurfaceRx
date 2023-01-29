5759e21d29e28561722976e40108ec1253848c40
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2023-01-28 19:19:30 -0500

    Added link to original code source

--- SurfaceRx/Core/Inc/retarget.h
+++ SurfaceRx/Core/Inc/retarget.h
@@ -15,6 +15,10 @@
 #include "stm32f3xx_hal.h"
 #include <sys/stat.h>
 
+// All credit to Carmine Noviello for this code
+// https://github.com/cnoviello/mastering-stm32/blob/master/nucleo-f030R8/system/include/retarget/retarget.h
+
+
 	void RetargetInit(UART_HandleTypeDef *huart);
 	int _isatty(int fd);
 	int _write(int fd, char* ptr, int len);
